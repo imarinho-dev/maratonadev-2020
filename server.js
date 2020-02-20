151c7750769510ccec3f8a952ca1eb40ab374ac9
imarinho-dev <imarinho.dev@gmail.com>
2020-02-20 09:36:46 -0300

    adicionando funcionalidades no servidor

--- server.js
+++ server.js
@@ -6,16 +6,59 @@ const PORT = 3000;
 // Configurar o servidor para apresentar arquivos estáticos
 server.use(express.static("public"));
 
+// Habilitar body do formulário
+
+server.use(express.urlencoded({
+  extended: true
+}));
+
 // Configurando a template engine
 const nunjucks = require("nunjucks");
 nunjucks.configure("./", {
-  express: server
+  express: server,
+  noCache: true
 });
+
+// Lista de doadores: Vetor ou Array
+const donors = [{
+    name: "Diego Fernandes",
+    blood: "AB+"
+  },
+  {
+    name: "Cleiton Souza",
+    blood: "B+"
+  },
+  {
+    name: "Robson Marques",
+    blood: "A+"
+  },
+  {
+    name: "Mayk Brito",
+    blood: "O+"
+  }
+];
 
 // Configurar a apresentação da página
 server.get("/", (req, res) => {
-  return res.render("index.html");
+  return res.render("index.html", {
+    donors
+  });
 });
+
+server.post("/", (req, res) => {
+  // pegar dados do formulário.
+  const name = req.body.name;
+  const email = req.body.email;
+  const blood = req.body.blood;
+
+  // Coloco valores dentro do Array
+  donors.push({
+    name: name,
+    blood: blood
+  });
+
+  return res.redirect("/");
+})
 
 // Ligar o servidor e permitir o acesso na porta 3000
 server.listen(PORT, () => {
